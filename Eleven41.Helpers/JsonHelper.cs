f15ad201e602a81c4f658bc9ebdf21c16dc5533e
mwhouser <matt@eleven41.com>
2014-05-07 23:06:58 -0400

    Use Newtonsoft Json.net for JsonHelper
    Closes #1

--- Eleven41.Helpers/JsonHelper.cs
+++ Eleven41.Helpers/JsonHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Text;
-using ServiceStack.Text;
+using Newtonsoft.Json;
 
 namespace Eleven41.Helpers
 {
@@ -9,27 +9,105 @@ namespace Eleven41.Helpers
 	{
 		public static string Serialize<T>(T obj)
 		{
-			return JsonSerializer.SerializeToString(obj, typeof(T));
+			if (obj == null)
+				throw new ArgumentNullException("obj");
+
+			using (StringWriter stringWriter = new StringWriter())
+			{
+				using (JsonTextWriter jsonTextWriter = new JsonTextWriter(stringWriter)
+					{
+						CloseOutput = false
+					})
+				{
+					var serializer = new JsonSerializer()
+					{
+						NullValueHandling = NullValueHandling.Ignore
+					};
+					serializer.Serialize(jsonTextWriter, obj);
+					jsonTextWriter.Flush();
+				}
+
+				return stringWriter.ToString();
+			}
 		}
 
 		public static string SerializeAndFormat<T>(T obj)
 		{
-			return obj.SerializeAndFormat();
+			if (obj == null)
+				throw new ArgumentNullException("obj");
+
+			using (StringWriter stringWriter = new StringWriter())
+			{
+				using (JsonTextWriter jsonTextWriter = new JsonTextWriter(stringWriter)
+					{
+						CloseOutput = false,
+						Formatting = Formatting.Indented
+					})
+				{
+					var serializer = new JsonSerializer()
+					{
+						NullValueHandling = NullValueHandling.Ignore
+					};
+					serializer.Serialize(jsonTextWriter, obj);
+					jsonTextWriter.Flush();
+				}
+
+				return stringWriter.ToString();
+			}
 		}
 
 		public static void SerializeToStream<T>(T obj, Stream stream)
 		{
-			JsonSerializer.SerializeToStream(obj, stream);
+			if (obj == null)
+				throw new ArgumentNullException("obj");
+			if (stream == null)
+				throw new ArgumentNullException("stream");
+
+			StreamWriter streamWriter = new StreamWriter(stream);
+			using (JsonTextWriter jsonTextWriter = new JsonTextWriter(streamWriter)
+				{
+					CloseOutput = false
+				})
+			{
+				var serializer = new JsonSerializer()
+				{
+					NullValueHandling = NullValueHandling.Ignore
+				};
+				serializer.Serialize(jsonTextWriter, obj);
+				jsonTextWriter.Flush();
+			}
+			streamWriter.Flush();
 		}
 
 		public static T Deserialize<T>(string json)
 		{
-			return JsonSerializer.DeserializeFromString<T>(json);
+			if (json == null)
+				throw new ArgumentNullException("json");
+
+			using (StringReader stringReader = new StringReader(json))
+			{
+				using (JsonTextReader jsonTextReader = new JsonTextReader(stringReader))
+				{
+					var serializer = new JsonSerializer();
+					return serializer.Deserialize<T>(jsonTextReader);
+				}
+			}
 		}
 
 		public static T DeserializeFromStream<T>(Stream stream)
 		{
-			return JsonSerializer.DeserializeFromStream<T>(stream);
+			if (stream == null)
+				throw new ArgumentNullException("stream");
+
+			StreamReader streamReader = new StreamReader(stream);
+			using (JsonTextReader jsonTextReader = new JsonTextReader(streamReader)
+				{
+					CloseInput = false
+				})
+			{
+				var serializer = new JsonSerializer();
+				return serializer.Deserialize<T>(jsonTextReader);
+			}
 		}
 	}
 }
